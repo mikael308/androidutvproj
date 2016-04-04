29cbadab6521452225b8cccef3db0c910030985b
mikael138 <mikael0612@hotmail.se>
2016-04-04 16:17:11 +0200

    init setting : language

--- app/src/main/java/com/example/mikael/androidutvproj/settings/SettingsListFragment.java
+++ app/src/main/java/com/example/mikael/androidutvproj/settings/SettingsListFragment.java
@@ -1,13 +1,18 @@
 package com.example.mikael.androidutvproj.settings;
 
+import android.app.AlertDialog;
 import android.app.ListFragment;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ScrollView;
 
 import com.example.mikael.androidutvproj.R;
 
@@ -28,6 +33,10 @@ public class SettingsListFragment extends ListFragment {
      * shared preferenes main key : settings
      */
     public static final String SHAREDPREFKEY_SETTINGS = "SETTINGS";
+    /**
+     * shared preferences key : this app language
+     */
+    public static final String SHAREDPREFKEY_LANGUAGE = "SHAREDPREFKEY_LANGUAGE";
 
     /**
      * this listview
@@ -37,6 +46,12 @@ public class SettingsListFragment extends ListFragment {
      * this listadapter
      */
     private SettingsListAdapter     mListAdapter;
+
+    /**
+     * listitem : language settigns
+     * @see #SHAREDPREFKEY_LANGUAGE
+     */
+    private SettingsListItem        mListitem_lang;
 
     /**
      * this shared preferences
@@ -74,6 +89,9 @@ public class SettingsListFragment extends ListFragment {
             @Override
             public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
                 switch (key) {
+                    case SHAREDPREFKEY_LANGUAGE:
+                        mListitem_lang.setInfo(sharedPreferences.getString(key, Lang.ENGLISH.getName()));
+                        break;
 
                 }
             }
@@ -87,6 +105,19 @@ public class SettingsListFragment extends ListFragment {
      */
     private void init_listitems(){
 
+        // LANGUAGE
+        ////////////////////////////////////
+        String lang_header  = getResources().getString(R.string.settings_listitem_language_header);
+        String lang_info    = mSharedPref.getString(SHAREDPREFKEY_LANGUAGE, Lang.ENGLISH.getName());
+        mListitem_lang      = new SettingsListItem(getActivity(), lang_header, lang_info);
+        mListitem_lang      .setInfo(lang_info);
+        mListitem_lang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog_language().show();
+            }
+        });
+
     }
     /**
      * initialize this adapter
@@ -98,11 +129,48 @@ public class SettingsListFragment extends ListFragment {
 
         // add items to adapter
         List<SettingsListItem> listitems = new ArrayList<>();
-
+        listitems.add(mListitem_lang);
+        listitems.add(mListitem_photosrc);
 
         mListAdapter = new SettingsListAdapter(getActivity().getApplicationContext(), R.layout.activity_settings);
         mListAdapter.addAll(listitems);
         mListView.setAdapter(mListAdapter);
+    }
+
+    /**
+     * settings dialog set app language
+     * if confirmed: shared preference {@link #SHAREDPREFKEY_LANGUAGE} is set to new value
+     * @return language dialog
+     * @see Lang
+     */
+    private AlertDialog dialog_language(){
+        ArrayList<String> listitems = new ArrayList<>();
+
+        LinearLayout layout = new LinearLayout(getActivity());
+        for (Lang l : Lang.values()){
+            listitems.add(l.getName());
+            layout.addView(new SettingsListItem(getActivity(), l.getName()));
+        }
+        ScrollView scrollView = new ScrollView(getActivity());
+        scrollView.addView(layout);
+
+        AlertDialog.Builder builder     = new AlertDialog.Builder(getActivity());
+        ArrayAdapter<String> adapter    = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, listitems);
+        return builder
+                .setTitle(getResources().getString(R.string.settings_listitem_language_header))
+                .setIcon(R.drawable.ic_language_black_24dp)
+                .setAdapter(adapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String lang = Lang.findByIdx(which).getName();
+                        getActivity().getSharedPreferences(SHAREDPREFKEY_SETTINGS, getActivity().MODE_PRIVATE).edit()
+                                .putString(SHAREDPREFKEY_LANGUAGE, lang)
+                                .apply();
+
+                    }
+                })
+                .setNegativeButton(getResources().getString(R.string.btn_cancel), null)
+                .create();
     }
 
 }
