ce482a453e47bce5c86868dc05366a0da41acdc4
mikael138 <mikael0612@hotmail.se>
2016-04-04 16:00:09 +0200

    rm unused imports

--- app/src/main/java/com/example/mikael/androidutvproj/eventlist/EventListFragment.java
+++ app/src/main/java/com/example/mikael/androidutvproj/eventlist/EventListFragment.java
@@ -5,8 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 
 import com.example.mikael.androidutvproj.event.Event;
 
