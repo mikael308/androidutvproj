1562073a30a70fe369fe03fcb41b73c2fd21f22a
mikael138 <mikael0612@hotmail.se>
2016-04-19 23:47:01 +0200

    init

---
+++ app/src/main/java/com/example/mikael/androidutvproj/apartment/DataAccessObject.java
@@ -0,0 +1,42 @@
+package com.example.mikael.androidutvproj.apartment;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * @author Mikael Holmbom
+ * @version 1.0
+ */
+public abstract class DataAccessObject implements Parcelable {
+
+    /**
+     * this id<br>
+     *     used as key in database
+     */
+    private String mId;
+
+
+    public DataAccessObject(String id){
+        mId = id;
+    }
+
+    public String getId(){
+        return mId;
+    }
+    protected void setId(String id){
+        mId = id;
+    }
+
+    public void writeToParcel(Parcel parcel, int i){
+        parcel.writeString(getId());
+    }
+
+    protected DataAccessObject(Parcel in){
+        String[] stringData   = new String[0];
+        in.readStringArray(stringData);
+        setId(stringData[0]);
+
+    }
+
+}
