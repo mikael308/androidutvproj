b9f52bf27a74d42a2651703f987979e5aa72b0b2
mikael138 <mikael0612@hotmail.se>
2016-05-05 00:37:02 +0200

    init

---
+++ app/src/main/java/com/example/mikael/androidutvproj/selector/list/OnListItemClickListener.java
@@ -0,0 +1,19 @@
+package com.example.mikael.androidutvproj.selector.list;
+
+/**
+ * interface used for callback function when using ListView
+ * @author Mikael Holmbom
+ * @version 1.0
+ */
+public interface OnListItemClickListener<T>{
+    /**
+     * on listitem click
+     * @param selectedItem selected item
+     */
+    void onItemClick(T selectedItem, int position);
+    /**
+     * on listitem long click
+     * @param selectedItem selected item
+     */
+    void onItemLongClick(T selectedItem, int position);
+}
