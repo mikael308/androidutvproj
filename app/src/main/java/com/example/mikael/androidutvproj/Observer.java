41376cc9e677e0eceaa2f84423afebbf399f9221
mikael138 <mikael0612@hotmail.se>
2016-05-14 22:11:25 +0200

    init

---
+++ app/src/main/java/com/example/mikael/androidutvproj/Observer.java
@@ -0,0 +1,13 @@
+package com.example.mikael.androidutvproj;
+
+import com.example.mikael.androidutvproj.activity.SubjectActivity;
+
+/**
+ * GoF-pattern
+ * @author Mikael Holmbom
+ * @version 1.0
+ * @see SubjectActivity
+ */
+public interface Observer {
+    void update();
+}
