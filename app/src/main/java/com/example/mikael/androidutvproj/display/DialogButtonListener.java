42605e2b517559de13da314eacb2529cb5f9ded8
mikael138 <mikael0612@hotmail.se>
2016-05-18 14:43:42 +0200

    javadoc fix

--- app/src/main/java/com/example/mikael/androidutvproj/display/DialogButtonListener.java
+++ app/src/main/java/com/example/mikael/androidutvproj/display/DialogButtonListener.java
@@ -10,27 +10,27 @@ package com.example.mikael.androidutvproj.display;
 public class DialogButtonListener {
 
     /**
-     * is called on positive button click<br>
+     * is called on Dialog positive button click<br>
      * return true if dialog status is ok and should be closed<br>
-     *     return false if dialog should be closed down
+     *     return false if dialog should stay visible
      * @return true if dialog status is ok
      */
     public boolean onPositiveClick(){
         return true;
     }
     /**
-     * is called on negative button click<br>
+     * is called on Dialog negative button click<br>
      * return true if dialog status is ok and should be closed<br>
-     *     return false if dialog should be closed down
+     *     return false if dialog should stay visible
      * @return true if dialog status is ok
      */
     public boolean onNegativeClick(){
         return true;
     }
     /**
-     * is called on neutral button click<br>
+     * is called on Dialog neutral button click<br>
      * return true if dialog status is ok and should be closed<br>
-     *     return false if dialog should be closed down
+     *     return false if dialog should stay visible
      * @return true if dialog status is ok
      */
     public boolean onNeutralClick(){
