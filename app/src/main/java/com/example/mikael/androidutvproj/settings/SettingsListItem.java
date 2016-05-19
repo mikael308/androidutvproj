0565fd9271aafb82ae63a4ff738e87063a424fba
mikael138 <mikael0612@hotmail.se>
2016-05-19 21:56:32 +0200

    rm section: info

--- app/src/main/java/com/example/mikael/androidutvproj/settings/SettingsListItem.java
+++ app/src/main/java/com/example/mikael/androidutvproj/settings/SettingsListItem.java
@@ -1,7 +1,6 @@
 package com.example.mikael.androidutvproj.settings;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -12,7 +11,6 @@ import com.example.mikael.androidutvproj.R;
  *     contains:
  *<ul>
  *     <li>header</li>
- *     <li>info</li>
  *</ul>
  *
  * @author Mikael Holmbom
@@ -25,53 +23,35 @@ class SettingsListItem extends LinearLayout {
      * settings listitem header
      */
     private TextView mHeader;
-    /**
-     * settings listitem info
-     */
-    private TextView mInfo;
+
 
     public SettingsListItem(Context context){
         super(context);
-        mHeader = new TextView(getContext());
-        mInfo = new TextView(getContext());
-
         init();
     }
     public SettingsListItem(Context context, String header){
         super(context);
-        mHeader = new TextView(getContext());
-        mHeader.setText(header);
-        mInfo = new TextView(getContext());
-
         init();
-    }
-    public SettingsListItem(Context context, String header, String info) {
-        super(context);
-        mHeader = new TextView(getContext());
-        mHeader.setText(header);
-        mInfo = new TextView(getContext());
-        mInfo.setText(info);
 
-        init();
+        mHeader.setText(header);
     }
 
     /**
      * initialize this object
      */
-    private void init(){
+    protected void init(){
         this.setOrientation(VERTICAL);
 
         LinearLayout headerLayout = new LinearLayout(getContext());
         headerLayout.setOrientation(HORIZONTAL);
 
-        mHeader.setTextSize(getResources().getDimension(R.dimen.settings_listitem_header_textsize));
-        mInfo.setTextColor(Color.BLACK);
-        mInfo.setTextSize(getResources().getDimension(R.dimen.settings_listitem_info_textsize));
-        mInfo.setPadding(getResources().getDimensionPixelSize(R.dimen.settings_listitem_info_padding_left), getResources().getDimensionPixelSize(R.dimen.settings_listitem_info_padding_top), getResources().getDimensionPixelSize(R.dimen.settings_listitem_info_padding_right), getResources().getDimensionPixelSize(R.dimen.settings_listitem_info_padding_bottom));
-        mInfo.setTextColor(Color.GRAY);
+        mHeader = new TextView(getContext());
+        mHeader.setTextAppearance(R.style.TextAppearance_AppCompat_Medium);
+        //TODO fixa så header blir MEDIUM TEXTVIEW
+        //mHeader.setTextSize(getResources().getDimension(R.dimen.settings_listitem_header_textsize));
+
 
         addView(mHeader);
-        addView(mInfo);
     }
 
     /**
@@ -81,13 +61,7 @@ class SettingsListItem extends LinearLayout {
     public String getHeader(){
         return mHeader.getText().toString();
     }
-    /**
-     * get this info string
-     * @return info text
-     */
-    public String getInfo(){
-        return mInfo.getText().toString();
-    }
+
     /**
      * set this header text string
      * @param newHeader new text value
@@ -97,14 +71,6 @@ class SettingsListItem extends LinearLayout {
         mHeader.setText(newHeader);
         return this;
     }
-    /**
-     * set this info text string
-     * @param newInfo new text value
-     * @return this SettingsListItem
-     */
-    public SettingsListItem setInfo(String newInfo){
-        mInfo.setText(newInfo);
-        return this;
-    }
+
 
 }
