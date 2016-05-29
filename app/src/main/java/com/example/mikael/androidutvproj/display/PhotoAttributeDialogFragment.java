4beec6e7c0cc9d35d64df40f00def3e9548bf2fe
mikael138 <mikael0612@hotmail.se>
2016-05-29 23:22:08 +0200

    refactor: use support DialogFragment

--- app/src/main/java/com/example/mikael/androidutvproj/display/PhotoAttributeDialogFragment.java
+++ app/src/main/java/com/example/mikael/androidutvproj/display/PhotoAttributeDialogFragment.java
@@ -2,10 +2,10 @@ package com.example.mikael.androidutvproj.display;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
