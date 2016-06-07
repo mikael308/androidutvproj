406d3400d59fef58b39b186b5e7653233666dbd8
miho1202 <miho1202@student.miun.se>
2016-06-07 23:26:00 +0200

    update imports

--- app/src/main/java/com/example/mikael/androidutvproj/PhotoFile.java
+++ app/src/main/java/com/example/mikael/androidutvproj/PhotoFile.java
@@ -3,7 +3,6 @@ package com.example.mikael.androidutvproj;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Environment;
 import android.util.Log;
 
 import com.example.mikael.androidutvproj.dao.RealEstate;
