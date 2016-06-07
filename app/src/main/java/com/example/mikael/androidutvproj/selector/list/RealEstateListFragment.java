96d5636d39756e6af0c249f215c0ee4b51d07606
miho1202 <miho1202@student.miun.se>
2016-06-07 22:07:57 +0200

    rm log

--- app/src/main/java/com/example/mikael/androidutvproj/selector/list/RealEstateListFragment.java
+++ app/src/main/java/com/example/mikael/androidutvproj/selector/list/RealEstateListFragment.java
@@ -144,7 +144,6 @@ public class RealEstateListFragment extends ClickableListFragment<RealEstateList
 
     @Override
     public void updateResetView() {
-        Log.d("listfragment", "resetView");
 
         RealEstateListAdapter listadapter = (RealEstateListAdapter) getListAdapter();
 
