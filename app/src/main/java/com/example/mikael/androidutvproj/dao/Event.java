9112297acf6fd0972d6fbcc8548d3f017efb3545
miho1202 <miho1202@student.miun.se>
2016-06-08 00:14:30 +0200

    update clone() prevent nullpointerexeption when receiving attr data

--- app/src/main/java/com/example/mikael/androidutvproj/dao/Event.java
+++ app/src/main/java/com/example/mikael/androidutvproj/dao/Event.java
@@ -172,9 +172,11 @@ public class Event extends ChildDataAccessObject implements Parcelable, Comparab
 
     @Override
     public Event clone() {
-        Event clone = new Event(getId())
-                .setDate(getDate())
-                .setForeignKey(getForeignKey());
+        Event clone = new Event(    getId())
+                .setForeignKey(     getForeignKey());
+
+        if(getDate() != null)
+            clone.setDate(          getDate());
 
         return clone;
     }
